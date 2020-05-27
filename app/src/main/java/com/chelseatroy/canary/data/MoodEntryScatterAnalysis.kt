bfa850ccc7ed35c67f92b905f1acc5fec79847dc
Carla Garcia <cdgarcia95@gmail.com>
2020-05-27 10:31:23 -0500

    Scatter analysis now accompanied by meaningful comment

--- app/src/main/java/com/chelseatroy/canary/data/MoodEntryScatterAnalysis.kt
+++ app/src/main/java/com/chelseatroy/canary/data/MoodEntryScatterAnalysis.kt
@@ -26,7 +26,75 @@ class MoodEntryScatterAnalysis() {
     }
 
     fun commentOn(moodEntries: List<MoodEntry>): String {
-        return "This text box is pretty useless so far. It would be great if this text box contained some useful information " +
-                "about the trends in my mood, wouldn't it?"
+        val averageMoodValues = getAverageMoodValues(moodEntries)
+        val singleOutlier = findSingleOutlier(moodEntries)
+
+            // Encouragement check
+        if (notEnoughEntries(moodEntries)) {
+            return "It's hard to glean much from just a few mood entries. Log more throughout the coming week to learn more about yourself!"
+        }
+
+        // Improving check
+        if (averageMoodValues.first < averageMoodValues.second && singleOutlier == null) {
+            return "Looks like your mood has been improving throughout the week! Take stock of what made you feel better, and stay smiling :)"
+        }
+
+        // Declining check
+        if (averageMoodValues.first > averageMoodValues.second && singleOutlier == null) {
+            return "Your mood has been on a bit of a downturn this week. It's a good time to stay mindful of the things you enjoy and the things you don't."
+        }
+
+        // Outlier check
+        if (singleOutlier != null) {
+            return "Seems like you felt ${singleOutlier} on a particular day this week. What happened that made you feel differently from the rest of the week?"
+        }
+
+        // Stable check
+        return "It looks like your mood has been pretty stable. Do you want that to change, or are you happy with how things are going right now?"
+    }
+
+    private fun notEnoughEntries(moodEntries: List<MoodEntry>): Boolean {
+        return moodEntries.size < 5
+    }
+
+    private fun getAverageMoodValues(moodEntries: List<MoodEntry>): Pair<Double, Double> {
+        val numEntries = moodEntries.size
+        val moodValues = moodEntries.map { moodEntry: MoodEntry -> getYPositionFor(moodEntry) }
+        val firstHalfAverage = moodValues.subList(0, numEntries/2 + 1).average()
+        val secondHalfAverage = moodValues.subList(numEntries/2, numEntries).average()
+        return Pair(firstHalfAverage, secondHalfAverage)
+    }
+
+    private fun findSingleOutlier(moodEntries: List<MoodEntry>): Mood? {
+        val moodValues = moodEntries.map { moodEntry: MoodEntry -> getYPositionFor(moodEntry) }
+        val average = moodValues.average()
+
+        val nonOutliersFromAverage =
+            moodValues.filter { it >= average - 2 && it <= average + 2 }
+        val outliersFromAverage =
+            moodValues.filter { it < average - 2 || it > average + 2 }
+
+        if (outliersFromAverage.size == 1) {
+            nonOutliersFromAverage.forEach {
+                if (outliersFromAverage[0] >= it - 1 && outliersFromAverage[0] <= it + 1) {
+                    return null
+                }
+            }
+            return getMoodFor(outliersFromAverage[0])
+        } else {
+            return null
+        }
+    }
+
+    private fun getMoodFor(yPosition: Float): Mood {
+        var mood: Mood = Mood.NEUTRAL
+        when (yPosition) {
+            1f -> mood = Mood.UPSET
+            2f -> mood = Mood.DOWN
+            3f -> mood = Mood.NEUTRAL
+            4f -> mood = Mood.COPING
+            5f -> mood = Mood.ELATED
+        }
+        return mood
     }
 }
--- app/src/test/java/com/chelseatroy/canary/data/MoodEntryScatterAnalysisTest.kt
+++ app/src/test/java/com/chelseatroy/canary/data/MoodEntryScatterAnalysisTest.kt
@@ -13,7 +13,7 @@ class MoodEntryScatterAnalysisTest {
     }
 
     @Test
-    fun arragesMoodsVertically_basedOnMoodHappiness() {
+    fun arrangesMoodsVertically_basedOnMoodHappiness() {
         assertEquals(1f, systemUnderTest.getYPositionFor(MoodEntry(Mood.UPSET)))
         assertEquals(2f, systemUnderTest.getYPositionFor(MoodEntry(Mood.DOWN)))
         assertEquals(3f, systemUnderTest.getYPositionFor(MoodEntry(Mood.NEUTRAL)))
@@ -36,31 +36,63 @@ class MoodEntryScatterAnalysisTest {
 
     @Test
     fun comments_whenThereArentManyMoodEntries_encourageMoreMoodLogging() {
-        fail("If the person hasn't logged their mood much, " +
-                "it wouldn't be very accurate to try to do any analysis." +
-                "So choose a minimum number of entries you think you need to make an analysis " +
-                "and produce a message encouraging more logging if there are fewer entries than that.")
+        val lessThanFiveMoodEntries = arrayListOf<MoodEntry>(
+            MoodEntry(Mood.ELATED, 1L, "Note", "EATING"),
+            MoodEntry(Mood.ELATED, 2L, "Note", "EATING"),
+            MoodEntry(Mood.ELATED, 3L, "Note", "EATING"),
+            MoodEntry(Mood.ELATED, 4L, "Note", "EATING")
+        )
+        val expectedEncouragingComment = "It's hard to glean much from just a few mood entries. Log more throughout the coming week to learn more about yourself!"
+        assertEquals(expectedEncouragingComment, systemUnderTest.commentOn(lessThanFiveMoodEntries))
+
+        val atLeastFiveMoodEntries = arrayListOf<MoodEntry>(
+            MoodEntry(Mood.ELATED, 1L, "Note", "EATING"),
+            MoodEntry(Mood.ELATED, 2L, "Note", "EATING"),
+            MoodEntry(Mood.ELATED, 3L, "Note", "EATING"),
+            MoodEntry(Mood.ELATED, 4L, "Note", "EATING"),
+            MoodEntry(Mood.ELATED, 5L, "Note", "EATING")
+        )
+        val expectedStableComment = "It looks like your mood has been pretty stable. Do you want that to change, or are you happy with how things are going right now?"
+        assertEquals(expectedStableComment, systemUnderTest.commentOn(atLeastFiveMoodEntries))
     }
 
     @Test
     fun comments_whenMoodIsImproving_mentionThatMoodIsImproving() {
-        fail("If the person's mood, in general, is better lately than it was earlier in the week " +
-                "produce a message mentioning it. " +
-                "You get to decide how you want to measure this.")
+        val improvingMoodEntries = arrayListOf<MoodEntry>(
+            MoodEntry(Mood.UPSET, 1L, "Note", "EATING"),
+            MoodEntry(Mood.DOWN, 2L, "Note", "EATING"),
+            MoodEntry(Mood.COPING, 3L, "Note", "EATING"),
+            MoodEntry(Mood.NEUTRAL, 4L, "Note", "EATING"),
+            MoodEntry(Mood.COPING, 5L, "Note", "EATING")
+        )
+        val expectedImprovingComment = "Looks like your mood has been improving throughout the week! Take stock of what made you feel better, and stay smiling :)"
+        assertEquals(expectedImprovingComment, systemUnderTest.commentOn(improvingMoodEntries))
     }
 
     @Test
     fun comments_whenMoodIsDeclining_mentionThatMoodIsDeclining() {
-        fail("If the person's mood, in general, is not as good lately as it was earlier in the week " +
-                "produce a message mentioning it. " +
-                "You get to decide how you want to measure this.")
+        val decliningMoodEntries = arrayListOf<MoodEntry>(
+            MoodEntry(Mood.ELATED, 1L, "Note", "EATING"),
+            MoodEntry(Mood.ELATED, 2L, "Note", "EATING"),
+            MoodEntry(Mood.ELATED, 3L, "Note", "EATING"),
+            MoodEntry(Mood.UPSET, 4L, "Note", "EATING"),
+            MoodEntry(Mood.DOWN, 5L, "Note", "EATING")
+        )
+        val expectedDecliningComment = "Your mood has been on a bit of a downturn this week. It's a good time to stay mindful of the things you enjoy and the things you don't."
+        assertEquals(expectedDecliningComment, systemUnderTest.commentOn(decliningMoodEntries))
     }
 
     @Test
     fun comments_whenThereIsOneOutlierMood_mentionAnyNotesFromThatMood() {
-        fail("If the person's mood, in general, is stable, and there is ONE outlier, " +
-                "produce a message containing the notes for that mood." +
-                "You get to decide how you want to go about this.")
+        val outlierMoodEntries = arrayListOf<MoodEntry>(
+            MoodEntry(Mood.ELATED, 1L, "Note", "EATING"),
+            MoodEntry(Mood.ELATED, 2L, "Note", "EATING"),
+            MoodEntry(Mood.UPSET, 3L, "Note", "EATING"),
+            MoodEntry(Mood.ELATED, 4L, "Note", "EATING"),
+            MoodEntry(Mood.ELATED, 5L, "Note", "EATING")
+        )
+        val expectedOutlierComment = "Seems like you felt UPSET on a particular day this week. What happened that made you feel differently from the rest of the week?"
+        assertEquals(expectedOutlierComment, systemUnderTest.commentOn(outlierMoodEntries))
     }
 
 
